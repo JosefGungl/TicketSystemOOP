ff29cc97c33ef6f9094ee6405318c104abc7019b
Josef Gungl <josef.gungl07@gmail.com>
2022-02-24 18:27:38 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicketSystemOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
